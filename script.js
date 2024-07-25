421f83c977ec9c7682c3e15dcc952ee3e27468a1
kili196 <kilian.baier16@gmail.com>
2024-07-25 09:10:36 +0200

    finished header and footer

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const card_container = document.getElementsByClassName("book-cards")
+
+
 function Book(title, desc, pages){
     this.title = title;
     this.desc = desc;
@@ -15,4 +18,31 @@ const book_6 = new Book("Book_1", "dsadjdsj sdajdsa kjd jdsak jdsa ask dsa kas j
 
 const book_arr = [book_1, book_2, book_3, book_4, book_5, book_6]
 
-console.log(book_arr)
+book_arr.forEach((e) => {
+    const div_card = document.createElement("div");
+    div_card.classList.add("book-card")
+  
+    const header = document.createElement("div");
+    const header_label = document.createElement("label");
+    header_label.innerHTML = e.title;
+    header.appendChild(header_label);
+
+    /** made divs and labels */
+    const information_section = document.createElement("div");
+    information_section.classList.add("information-section")
+    const book_info = document.createElement("label")
+    book_info.innerHTML = "Book-Info"
+    const divider = document.createElement("div")
+    divider.classList.add("divider")
+    const book_desc = document.createElement("p")
+    book_desc.innerHTML = e.desc
+
+    /** adding to information section */
+    information_section.appendChild(book_info);
+    information_section.appendChild(divider)
+    information_section.appendChild(book_desc)
+    information_section.appendChild(divider)
+    
+  
+})
+
