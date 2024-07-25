88d95e9395542ac6f89c25714a4b6d48421c4f7e
kili196 <kilian.baier16@gmail.com>
2024-07-25 08:47:31 +0200

    added book array

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ function Book(title, desc, pages){
     this.desc = desc;
     this.pages = pages; 
 }
+
+
+const book_1 = new Book("Book_1", "dsadjdsj sdajdsa kjd jdsak jdsa ask dsa kas jkdsadjk asjkd dsakj djadsjk asdkj asjk dasjkd", 250);
+const book_2 = new Book("Book_1", "dsadjdsj sdajdsa kjd jdsak jdsa ask dsa kas jkdsadjk asjkd dsakj djadsjk asdkj asjk dasjkd", 250);
+const book_3 = new Book("Book_1", "dsadjdsj sdajdsa kjd jdsak jdsa ask dsa kas jkdsadjk asjkd dsakj djadsjk asdkj asjk dasjkd", 250);
+const book_4 = new Book("Book_1", "dsadjdsj sdajdsa kjd jdsak jdsa ask dsa kas jkdsadjk asjkd dsakj djadsjk asdkj asjk dasjkd", 250);
+const book_5 = new Book("Book_1", "dsadjdsj sdajdsa kjd jdsak jdsa ask dsa kas jkdsadjk asjkd dsakj djadsjk asdkj asjk dasjkd", 250);
+const book_6 = new Book("Book_1", "dsadjdsj sdajdsa kjd jdsak jdsa ask dsa kas jkdsadjk asjkd dsakj djadsjk asdkj asjk dasjkd", 250);
+
+
+const book_arr = [book_1, book_2, book_3, book_4, book_5, book_6]
+
+console.log(book_arr)
