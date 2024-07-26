921996174b507d8ef98ece1d065977b23e2353e6
kili196 <kilian.baier16@gmail.com>
2024-07-26 14:34:04 +0200

    adding book with dialog working

--- script.js
+++ script.js
@@ -26,11 +26,13 @@ const book_6 = new Book("Book_1", "dsadjdsj sdajdsa kjd jdsak jdsa ask dsa kas j
 
 const book_arr = [book_1, book_2, book_3, book_4, book_5, book_6]
 
-book_arr.forEach((e) => {
+
+
+function addBookCard(bookObj){
     const div_card = document.createElement("div");
     div_card.classList.add("book-card")
 
-    if (e.read === true) {
+    if (bookObj.read === true) {
         div_card.style.border = "2px solid green";
     }
     else {
@@ -41,7 +43,7 @@ book_arr.forEach((e) => {
     const header = document.createElement("div");
     header.classList.add("header")
     const header_label = document.createElement("label");
-    header_label.innerHTML = e.title;
+    header_label.innerHTML = bookObj.title;
     header.appendChild(header_label);
 
     /** made divs and labels */
@@ -52,7 +54,7 @@ book_arr.forEach((e) => {
     const divider = document.createElement("div")
     divider.classList.add("divider")
     const book_desc = document.createElement("p")
-    book_desc.innerHTML = e.desc
+    book_desc.innerHTML = bookObj.desc
 
     /** adding to information section */
     information_section.appendChild(book_info);
@@ -70,7 +72,7 @@ book_arr.forEach((e) => {
     label_info.classList.add("page-header");
     label_info.innerHTML = "Pages: ";
     const pages = document.createElement("label");
-    pages.innerHTML = e.pages;
+    pages.innerHTML = bookObj.pages;
     footer_div.appendChild(label_info);
     footer_div.appendChild(pages);
 
@@ -93,24 +95,30 @@ book_arr.forEach((e) => {
     })
 
     div_card.addEventListener("click", function() {
-        div_card.style.border = e.read ? "2px solid green" : "2px solid red";
-        e.read = !e.read;
+        div_card.style.border = bookObj.read ? "2px solid green" : "2px solid red";
+        bookObj.read = !bookObj.read;
     })
 
+}
+
+book_arr.forEach((e) => {
+   addBookCard(e);
 
 })
 
 addNewBook[0].addEventListener("click", () => {
    
-    modalDialog[0].showModal()
+    modalDialog[0].showModal();
 })
 
 
 add_book_modal[0].addEventListener("click", () => {
-    book_title = document.getElementById("book-title")
-    book_desc = document.getElementById("book-description")
-    book_pages = document.getElementById("book-pages")
-    console.log(book_title)
+    book_title = document.getElementById("book-title");
+    book_desc = document.getElementById("book-description");
+    book_pages = document.getElementById("book-pages");
+    book_read = document.getElementById("book-read");
+    const newBook = book_read == 1 ? new Book(book_title.value, book_desc.value, book_pages.value, true) :new Book(book_title.value, book_desc.value, book_pages.value, false)
+    addBookCard(newBook);
 
 })
 
